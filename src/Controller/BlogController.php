18a696306302a61c91df702439e0c80e2a273447
zoumana <zoumana.bakayako@etu.univ-st-etienne.fr>
2025-12-14 16:42:36 +0100

    update

--- src/Controller/BlogController.php
+++ src/Controller/BlogController.php
@@ -8,10 +8,10 @@ use Symfony\Component\Routing\Attribute\Route;
 
 final class BlogController extends AbstractController
 {
-    #[Route('/blog', name: 'app_blog')]
-    public function index(): Response
+    #[Route('/blog/Contacts', name: 'app_blog')]
+    public function Contacts(): Response
     {
-        return $this->render('blog/index.html.twig', [
+        return $this->render('blog/Contacts.html.twig', [
             'controller_name' => 'BlogController',
         ]);
     }
@@ -23,10 +23,10 @@ final class BlogController extends AbstractController
             'controller_name' => 'BlogController',
         ]);
     }
-    #[Route('/blog/CV', name: 'app_CV')]
-    public function CV(): Response
+    #[Route('/blog/Apropos', name: 'app_Apropos')]
+    public function Apropos(): Response
     {
-        return $this->render('blog/CV.html.twig', [
+        return $this->render('blog/Apropos.html.twig', [
             'controller_name' => 'BlogController',
         ]);
     }
@@ -43,6 +43,30 @@ final class BlogController extends AbstractController
     public function Compétences(): Response
     {
         return $this->render('blog/Compétences.html.twig', [
+            'controller_name' => 'BlogController',
+        ]);
+    }
+
+    #[Route('/Compétence1', name: 'app_Compétence1')]
+    public function Compétence1(): Response
+    {
+        return $this->render('/Compétence1.html.twig', [
+            'controller_name' => 'BlogController',
+        ]);
+    }
+
+    #[Route('/Compétence2', name: 'app_Compétence2')]
+    public function Compétence2(): Response
+    {
+        return $this->render('/Compétence2.html.twig', [
+            'controller_name' => 'BlogController',
+        ]);
+    }
+
+    #[Route('/Compétence3', name: 'app_Compétence3')]
+    public function Compétence3(): Response
+    {
+        return $this->render('/Compétence3.html.twig', [
             'controller_name' => 'BlogController',
         ]);
     }
